cf44015dcdd2965ca4de6fddf6538576fc6fe22c
Yiwen Wang <paprikaiec@gmail.com>
2025-07-06 19:27:27 +0800

    ready to submit my lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,13 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page.
+    if((p->usyscall= (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -148,7 +155,6 @@ found:
     return 0;
   }
 
-
   return p;
 }
 
@@ -161,6 +167,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if (p->usyscall)
+    kfree((void*) (p->usyscall));
+  p->usyscall =0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -180,9 +189,7 @@ pagetable_t
 proc_pagetable(struct proc *p)
 {
   pagetable_t pagetable;
-  struct usyscall* ucall;
   
-
   // An empty page table.
   pagetable = uvmcreate();
   if(pagetable == 0)
@@ -206,15 +213,9 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-   // map the usycall
-  if ((ucall = (struct usyscall*) kalloc()) == 0) {
-    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
-    uvmfree(pagetable, 0);
-    return 0;
-  }
 
   if (mappages(pagetable, USYSCALL, PGSIZE, 
-               (uint64) ucall, PTE_R | PTE_U) < 0) {
+               (uint64)(p->usyscall), PTE_R | PTE_U) < 0) {
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
     uvmunmap(pagetable, USYSCALL, 1, 0);
     uvmfree(pagetable, 0);
@@ -231,7 +232,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
-  uvmunmap(pagetable, USYSCALL, 1, 1);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
